f5c585e33c6808963a6f3e45db9ed312e83e6db5
Charlene Couch <charlene.couch15@pcc.edu>
2017-10-23 20:57:29 -0700

    added date

--- src/cis233j/helloworld/HelloWorld.java
+++ src/cis233j/helloworld/HelloWorld.java
@@ -1,4 +1,5 @@
 package cis233j.helloworld;
+import java.util.Date;
 
 /**
  * Created by Charlene Couch 10/23/17
@@ -7,5 +8,6 @@ package cis233j.helloworld;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println ("Hello World, I'm Back!");
+        System.out.println (new Date());
     }
 }
