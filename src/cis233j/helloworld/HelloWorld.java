1fbf78800d5311aca65a68b326b13b8d84db9f4e
Charlene Couch <charlene.couch15@pcc.edu>
2017-10-23 20:49:39 -0700

    added main method that prints a hello statment

--- src/cis233j/helloworld/HelloWorld.java
+++ src/cis233j/helloworld/HelloWorld.java
@@ -1,4 +1,11 @@
 package cis233j.helloworld;
 
+/**
+ * Created by Charlene Couch 10/23/17
+ */
+
 public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println ("Hello World, I'm Back!");
+    }
 }
